b6da2d28a5d0e1e97ff1abf8c23a68bdb78ba03e
tradaduong2811 <htluan2811@gmail.com>
2016-06-16 15:45:37 +0700

    commit

---
+++ Library/Models/Sach.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Sach
+    {
+        public int SachId { get; set; }
+        public string TenSach { get; set; }
+    }
+}
