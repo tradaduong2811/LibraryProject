864254b9f4e71eb83550d6df0acab2c939af97f8
Nam <nampham7394@gmail.com>
2016-06-30 17:45:09 +0700

    Commit Final

---
+++ Library/Controllers/BackupController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class BackupController
+    {
+
+        public void Backup(string database, string path)
+        {
+            SqlConnection con = new SqlConnection();
+            con = Provider.ConnectionData();
+    
+            string sql = "BACKUP DATABASE " + "[" +database+ "]" + " TO DISK = '" + path + "\\" + database + DateTime.Now.Ticks.ToString() + ".bak'";
+            SqlCommand command = new SqlCommand(sql, con);
+            command.ExecuteNonQuery();
+            
+            con.Close();
+            con.Dispose();
+        }
+        public SqlDataReader LoadDatabase()
+        {
+            SqlConnection con = new SqlConnection();
+            con = Provider.ConnectionData();      
+            
+            SqlCommand command = new SqlCommand("sp_databases", con);
+            SqlDataReader reader = command.ExecuteReader();
+            
+            return reader;
+        }
+    }
+}
---
+++ Library/Controllers/RestoreController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Controllers
+{
+    public class RestoreController
+    {
+        public void Restore(string path)
+        {   
+
+            SqlConnection conn = new SqlConnection();
+            conn = Provider.ConnectionData();
+            String sql = "";
+            sql = "USE master;";
+            sql += "Alter Database [2016-ProjectHQT-7-2012] Set SINGLE_USER WITH ROLLBACK IMMEDIATE;";
+            sql += "Restore Database [2016-ProjectHQT-7-2012] FROM Disk = '" + path + "' WITH REPLACE;";
+            SqlCommand command = new SqlCommand(sql, conn);
+            command.ExecuteNonQuery();
+
+            string Alter2 = @"ALTER DATABASE [2016-ProjectHQT-7-2012] SET Multi_User";
+            SqlCommand Alter2Cmd = new SqlCommand(Alter2, conn);
+            conn.Close();
+            conn.Dispose();
+        }
+                     
+    }
+}
